f6b79855b419cabca77b11a37e40f481c97bbfdd
dinerozz <dinerozz@users.noreply.github.com>
2021-03-31 15:19:36 +0600

    1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// var prevScrollpos = window.pageYOffset;
+// // document.classList.toggle('header-links');
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "-150px";
+//   } else {
+//     document.getElementById("navbar").style.top = "0";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }

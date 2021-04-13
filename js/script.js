d45f45b140250c92916ab5c6168d7bdc89f37d1f
dinerozz <dinerozz@users.noreply.github.com>
2021-04-13 17:15:19 +0600

    ssss

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-var prevScrollpos = window.pageYOffset;
-// document.classList.toggle('header-links');
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "-150px";
-  } else {
-    document.getElementById("navbar").style.top = "0";
-  }
-  prevScrollpos = currentScrollPos;
-}
+const menu = document.querySelector(".header-icon");
+const ul = document.querySelector(".header-ul");
+const span1 = document.querySelector(".line-1");
+const span2 = document.querySelector(".line-2");
+const span3 = document.querySelector(".line-3");
+
+menu.addEventListener('click',()=>{
+    ul.classList.toggle('active');
+    span1.classList.toggle('span1-style');
+    span2.classList.toggle('span2-style');
+    span3.classList.toggle('span3-style');
+})
